7f760fb4a3e2ffea765f893187d39ad56865527d
StoneFree <seraphinjraphy@gmail.com>
2026-03-24 23:48:56 +0530

    reconnection with exponential backoff added

--- agent/agent.go
+++ agent/agent.go
@@ -5,21 +5,47 @@ import (
 	"io"
 	"log"
 	"net"
+	"time"
 
 	"github.com/hashicorp/yamux"
 )
 
 func main() {
 	fmt.Println("Starting Local Agent")
+
+	baseDelay := 1 * time.Second
+	maxDelay := 30 * time.Second
+	currentDelay := baseDelay
+
+	for {
+		err := runAgent(&currentDelay, baseDelay)
+		if err != nil {
+			fmt.Printf("Connection lost or failed:%v\n", err)
+			fmt.Printf("Retrying in %v...\n", currentDelay)
+			time.Sleep(currentDelay)
+
+			currentDelay *= 2
+			if currentDelay > maxDelay {
+				currentDelay = maxDelay
+			}
+		}
+	}
+
+}
+
+func runAgent(currentDelay *time.Duration, baseDelay time.Duration) error {
 	relayConn, err := net.Dial("tcp", "localhost:7000")
 	if err != nil {
-		log.Fatalf("Could not connect to Relay: %v", err)
+		return err
 	}
+	defer relayConn.Close()
+
 	fmt.Println("Connected to relay")
+	*currentDelay = baseDelay
 
 	session, err := yamux.Server(relayConn, nil)
 	if err != nil {
-		log.Fatal(err)
+		return fmt.Errorf("Yamux initialization failed: %v", err)
 	}
 
 	for {
